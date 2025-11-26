2172bc3b4e00d91ff0f10450b255ada0e8ddc1b5
Allen Bouchard <allen.bouchard@gmail.com>
2025-11-26 12:19:56 -0500

    update existing unit tests for new project/module model

--- src/test_cocomo.py
+++ src/test_cocomo.py
@@ -4,26 +4,17 @@ from cocomo import *
 
 class TestCocomo(unittest.TestCase):
     
-    
-    
-    
-    
-    
-    
-    def test_csci_child_estimate_schedule(self):
-        child: CsciChild = CsciChild("Test CSCI")
-        child.ksloc = 100
-        child.em_prod = 1.0
-        child.sf_sum = 24.0
+    def test_module_estimate_schedule(self):
+        module: Module = Module("<--test-->")
+        module.ksloc = 100
+        module.em_prod = 1.0
+        scale_factor: float = 24.0
         
-        child.estimate_schedule()
-        self.assertAlmostEqual(586.61, child.nominal_schedule, places=2)
-        
-
-    
+        module.estimate_schedule(scale_factor)
+        self.assertAlmostEqual(586.61, module.nominal_schedule, places=2)
 
     def test_calculate_function_points(self):
-        child: CsciChild = CsciChild("Test CSCI")
+        module: Module = Module("<--test-->")
         
         counts = {
             "EI": (5, 10, 2),
@@ -33,15 +24,15 @@ class TestCocomo(unittest.TestCase):
             "EQ":  (2, 5, 0),
         }
         
-        child.calculate_function_points(counts)
-        self.assertEqual(654, child.function_points)
+        module.calculate_function_points(counts)
+        self.assertEqual(654, module.function_points)
 
     def test_function_points_to_ksloc(self):
-        child: CsciChild = CsciChild("Test CSCI")
-        child.function_points = 100
-        child.language = "C"
-        child.function_points_to_ksloc()
-        self.assertEqual(12_800, child.ksloc)
+        module: Module = Module("<--test-->")
+        module.function_points = 100
+        module.language = "C"
+        module.function_points_to_ksloc()
+        self.assertEqual(12_800, module.ksloc)
 
 
 

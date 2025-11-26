aadd353b6ee54da8a2c74459a4c5b7402c82d4ce
Allen Bouchard <allen.bouchard@gmail.com>
2025-11-25 22:25:18 -0500

    add scale_factors and move to top level parent

--- src/cocomo.py
+++ src/cocomo.py
@@ -1,34 +1,57 @@
 from typing import Union
 import json
 from constants import *
+from cocomo_tables import RatingLevel
 
 class Csci:
     def __init__(self, title: str):
         self.title = title
         self.nominal_schedule: float = 0.0
         self.parent = None
-        
-    
+        self.__scale_factors: dict[str, str] = {}
 class CsciParent(Csci):
-    def __init__(self, title: str, children: list[Csci]=[]):
+    def __init__(self, title: str, children: list[Csci]=[], parent: Union["CsciParent", None]=None):
         super().__init__(title)
         self.children: list[Csci] = children
+        
+        if self.parent is None:
+            self.__scale_factors = {"PREC": RatingLevel.NOMINAL,
+                                    "FLEX": RatingLevel.NOMINAL,
+                                    "RESL": RatingLevel.NOMINAL,
+                                    "TEAM": RatingLevel.NOMINAL,
+                                    "PMAT": RatingLevel.NOMINAL,
+                                    }
+        else:
+            self.__scale_factors = {}
 
     def add_child(self, child:Csci) -> None:
         if child not in self.children:
             self.children.append(child)
         
-        self.__private_update_estimate_schedule(child.nominal_schedule)
+        self.__update_estimate_schedule(child.nominal_schedule)
         
     def remove_child(self, child: Csci) -> Union[Csci, None]:
         
         for i in range(0, len(self.children)):
             if self.children[i] is child:
-                self.__private_update_estimate_schedule(-child.nominal_schedule)
+                self.__update_estimate_schedule(-child.nominal_schedule)
                 return self.children.pop(i)      
         return None
     
-    def __private_update_estimate_schedule(self, add_schedule: float) -> None:
+    def update_scale_factors(self, factors: dict[str. str]) -> None:
+        if self.parent is None:
+            raise Exception("scale factors must be added to top level CSCI")
+        
+        self.__scale_factors = factors
+    
+    def get_scale_factors(self) -> dict[str, str]:
+        if self.parent is None:
+            return self.__scale_factors
+        else:
+            return self.parent.get_scale_factors()
+    
+    
+    def __update_estimate_schedule(self, add_schedule: float) -> None:
         self.nominal_schedule += add_schedule
         if self.parent is not None:
             self.parent.__private_update_estimate_schedule(self.nominal_schedule)
@@ -56,7 +79,7 @@ class CsciChild(Csci):
         E: float = B + 0.01 * self.sf_sum
         self.nominal_schedule = A * self.ksloc**E * self.em_prod
         if self.parent is not None:
-            self.parent.__private_update_estimate_schedule(self.nominal_schedule)
+            self.parent.__update_estimate_schedule(self.nominal_schedule)
         
 
     def calculate_function_points(self, function_counts: dict[str, tuple[int, int, int]]) -> None:
---
+++ src/cocomo_tables.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class RatingLevel(Enum):
+    VERY_LOW    = "Very Low"
+    LOW         = "Low"
+    NOMINAL     = "Nominal"
+    HIGH        = "High"
+    VERY_HIGH   = "Very High"

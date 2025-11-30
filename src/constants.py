031fe26de882ef0a21adee0a71c1d265a6edea4b
Allen Bouchard <allen.bouchard@gmail.com>
2025-11-30 13:21:14 -0500

    finish entering effort modifier and scale factor values incot constants.py

--- src/constants.py
+++ src/constants.py
@@ -20,6 +20,15 @@ class ScaleFactor(Enum):
     RESL = "Risk Resolution"
     TEAM = "Team Cohesion"
     PMAT = "Process Maturity"
+    
+SCALE_FACTOR_VALUES = {
+    ScaleFactor.PREC: {RatingLevel.VERY_LOW: 6.20, RatingLevel.LOW: 4.96, RatingLevel.NOMINAL: 3.72, RatingLevel.HIGH: 2.48, RatingLevel.HIGH: 1.24, RatingLevel.EXTRA_HIGH: 0.00},
+    ScaleFactor.FLEX: {RatingLevel.VERY_LOW: 5.07, RatingLevel.LOW: 4.05, RatingLevel.NOMINAL: 3.04, RatingLevel.HIGH: 2.03, RatingLevel.HIGH: 1.01, RatingLevel.EXTRA_HIGH: 0.00},
+    ScaleFactor.RESL: {RatingLevel.VERY_LOW: 7.07, RatingLevel.LOW: 5.65, RatingLevel.NOMINAL: 4.24, RatingLevel.HIGH: 2.83, RatingLevel.HIGH: 1.41, RatingLevel.EXTRA_HIGH: 0.00},
+    ScaleFactor.TEAM: {RatingLevel.VERY_LOW: 5.48, RatingLevel.LOW: 4.38, RatingLevel.NOMINAL: 3.29, RatingLevel.HIGH: 2.19, RatingLevel.HIGH: 1.10, RatingLevel.EXTRA_HIGH: 0.00},
+    ScaleFactor.PMAT: {RatingLevel.VERY_LOW: 7.80, RatingLevel.LOW: 6.24, RatingLevel.NOMINAL: 4.68, RatingLevel.HIGH: 3.12, RatingLevel.HIGH: 1.56, RatingLevel.EXTRA_HIGH: 0.00},
+    
+}
 
 class EffortModifier(Enum):
     RELY = "Required Reliability"
@@ -40,28 +49,26 @@ class EffortModifier(Enum):
     SITE = "Multisite Development"
     SCED = "Required Development Schedule"
 
-SCALE_FACTOR_COST_DRIVERS = {
+EFFORT_MODIFIER_COST_DRIVERS = {
     EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.10, RatingLevel.HIGH: 1.26},
     EffortModifier.DATA: {                            RatingLevel.LOW: 0.90, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.14, RatingLevel.HIGH: 1.28},
     EffortModifier.CPLX: {RatingLevel.VERY_LOW: 0.73, RatingLevel.LOW: 0.87, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.17, RatingLevel.VERY_HIGH: 1.34, RatingLevel.EXTRA_HIGH: 1.74},
     EffortModifier.RUSE: {                            RatingLevel.LOW: 0.95, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.07, RatingLevel.VERY_HIGH: 1.15, RatingLevel.EXTRA_HIGH: 1.24},
     EffortModifier.DOCU: {RatingLevel.VERY_LOW: 0.81, RatingLevel.LOW: 0.91, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.11, RatingLevel.VERY_HIGH: 1.23},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
-    EffortModifier.RELY: {RatingLevel.VERY_LOW: 0.82, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26},
+    EffortModifier.TIME: {                                                   RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.11, RatingLevel.VERY_HIGH: 1.29, RatingLevel.EXTRA_HIGH: 1.63},
+    EffortModifier.STOR: {                                                   RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.05, RatingLevel.VERY_HIGH: 1.17, RatingLevel.EXTRA_HIGH: 1.46},
+    EffortModifier.PVOL: {                            RatingLevel.LOW: 0.87, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.15, RatingLevel.VERY_HIGH: 1.30},
+    EffortModifier.ACAP: {RatingLevel.VERY_LOW: 1.42, RatingLevel.LOW: 1.19, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.85, RatingLevel.VERY_HIGH: 0.71},
+    EffortModifier.PCAP: {RatingLevel.VERY_LOW: 1.34, RatingLevel.LOW: 1.15, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.88, RatingLevel.VERY_HIGH: 0.76},
+    EffortModifier.PCON: {RatingLevel.VERY_LOW: 1.29, RatingLevel.LOW: 1.12, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.90, RatingLevel.VERY_HIGH: 0.81},
+    EffortModifier.APEX: {RatingLevel.VERY_LOW: 1.22, RatingLevel.LOW: 1.10, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.88, RatingLevel.VERY_HIGH: 0.81},
+    EffortModifier.PLEX: {RatingLevel.VERY_LOW: 1.19, RatingLevel.LOW: 1.09, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.91, RatingLevel.VERY_HIGH: 0.85},
+    EffortModifier.LTEX: {RatingLevel.VERY_LOW: 1.20, RatingLevel.LOW: 1.09, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.91, RatingLevel.VERY_HIGH: 0.84},
+    EffortModifier.TOOL: {RatingLevel.VERY_LOW: 1.17, RatingLevel.LOW: 1.09, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.90, RatingLevel.VERY_HIGH: 0.78},
+    EffortModifier.SITE: {RatingLevel.VERY_LOW: 1.22, RatingLevel.LOW: 1.09, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 0.93, RatingLevel.VERY_HIGH: 0.86, RatingLevel.EXTRA_HIGH: 0.80},
 }
 
-
-
-
+SCHEDULE_COST_DRIVER = {RatingLevel.VERY_LOW: 1.43, RatingLevel.LOW: 0.92, RatingLevel.NOMINAL: 1.00, RatingLevel.HIGH: 1.26, RatingLevel.VERY_HIGH: 1.26, RatingLevel.EXTRA_HIGH: 1.24}
 
 
 
